4845fa43e3673dc117248150b440a83cd5003b64
Egor Romanoff <egoromanoff@mail.ru>
2022-11-28 14:16:01 +0300

    feat: add responsive to tabs on team page

--- pages/team/index.jsx
+++ pages/team/index.jsx
@@ -183,7 +183,6 @@ function Team() {
 {/* Back button */}
       <Button
         className="btn btn--back my-30"
-        // href='/#brief'
         onClick={() => router.back()}
       >
         <svg className='btn__icon' width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
