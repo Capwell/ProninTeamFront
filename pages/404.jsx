155a284e97207fcfcc871cde23cf4dfb9ce01587
Egor Romanov <67374276+EgoRomanoff@users.noreply.github.com>
2022-12-03 13:51:39 +0300

    Dev (#8)
    * init: init frontend of ProninTeam
    
    * fix: fix console.logs
    
    * refactor: change content, rewrite styles
    
    * feat: add responsive to tabs on team page
    
    * fix: fix overflowing on Legal page
    
    * feat: add video modal template
    
    * fix: change form submit
    
    * feat: add team members data
    
    * feat: update packages
    
    * feat: create PTButton component
    
    * feat: add button for removing attached file
    
    * feat: change header and footer links
    
    * feat: create cases page template
    
    * feat: add styled-components using for cases btns
    
    * feat: add :active to styled-components
    
    * feat: add gradient to cases items
    
    * feat: create CaseBanner component
    
    * feat: update PTButton component
    
    * feat: add default loader component for button
    
    * refactor: common changes on all pages
    
    * refactor: rewrite color pallette
    
    * feat: create custom loader
    
    * feat: add api functions
    
    * fix: change member photo aspect ratio
    
    * feat: create pronin-team case page
    
    * feat: add responses with static props method
    
    * fix: remove mock data
    
    * feat: add some tests and fix fetches
    
    * debug: debug deploy script
    
    * debug: debug deploy script
    
    * debug: debug deploy script
    
    * debug: debug deploy script
    
    * feat: add some tests
    
    * debug: debug deploy script
    
    * fix: bugfix
    
    * feat: add tests
    
    * feat: add error text on file input
    
    * feat: add fetch case on main page
    
    * fix: fix main page api request
    
    * fix: change offer post request
    
    * feat: add acepted file types on form
    
    * feat: change accepted file types on form
    
    * feat: add 404 and 500 pages
    
    * feat: fix test's mistakes
    
    * refactor: change pallette
    
    * feat: change 404 styles
    
    * fix: fix page height bugs
    
    Co-authored-by: Grikhanov Maxim <maximw33@yandex.ru>

--- pages/404.jsx
+++ pages/404.jsx
@@ -8,7 +8,8 @@ function Custom404() {
       <PTHead
         title="ProninTeam - 404: Страница не найдена"
       />
-      <Container fluid="xxl" className={ stl.wrapper }>
+
+      <Container fluid="xxl" className={ `${stl.wrapper} mt-100 mb-115` }>
         <h1 className={ stl.title }>404</h1>
         <span className={ stl.description }>
           Страница не найдена или еще не создана
