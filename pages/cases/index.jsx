6a3f2fe2fbba69ea3c7aaf8ff31d8e71ec0bcd86
Egor Romanoff <egoromanoff@mail.ru>
2022-12-05 19:25:22 +0300

    fix

--- pages/cases/index.jsx
+++ pages/cases/index.jsx
@@ -4,24 +4,56 @@ import PTHead from '../../components/PTHead/PTHead'
 import PTButton from '../../components/PTButton/PTButton'
 import stl from '../../styles/Cases.module.scss'
 import CaseBanner from '../../components/CaseBanner/CaseBanner'
-import api from '../../utils/api'
+// import api from '../../utils/api'
 
-export async function getStaticProps() {
-  let casesData = []
+// export async function getStaticProps() {
+//   let casesData = []
 
-  try {
-    const allData = await api.getCases()
-    // filter only visible cases
-    casesData = allData.filter(caseData => caseData.is_visible === true)
-  } catch (err) {
-    console.log(err.message)
+//   try {
+//     const allData = await api.getCases()
+//     // filter only visible cases
+//     casesData = allData.filter(caseData => caseData.is_visible === true)
+//   } catch (err) {
+//     console.log(err.message)
+//   }
+
+//   return { props: { casesData } }
+// }
+
+const casesData = [
+  {
+    title: 'DonorSearch',
+    hex_color: '#FF2A23',
+    logo: '/images/donorsearch-logo.svg',
+    is_on_main_page: false,
+    is_visible: true,
+    slug: 'donor-search',
+    text: 'Закрываем основные IT-потребности заказчика, накопившиеся за 12-ти летнюю историю проекта.'
+  },
+  {
+    title: 'Unity',
+    hex_color: '#FF2A23',
+    logo: '/images/unity-logo.svg',
+    is_on_main_page: false,
+    is_visible: true,
+    slug: 'unity',
+    text: 'Разработали раздел для популяризации донорства.'
+  },
+  {
+    title: 'ProninTeam',
+    hex_color: '#30D5C8',
+    logo: '/images/proninteam-logo.svg',
+    is_on_main_page: true,
+    is_visible: true,
+    slug: 'pronin-team',
+    text: 'Разработали сайт компании ProninTeam с нуля за 4 недели.',
   }
+]
 
-  return { props: { casesData } }
-}
 
 // function Cases({ casesData = [] }) {
-function Cases({ casesData }) {
+// function Cases({ casesData }) {
+function Cases() {
   const router = useRouter()
 
   return (
