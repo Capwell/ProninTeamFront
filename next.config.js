e83c4916b0f01fc0414e369c33c91598545ef8c1
EgoRomanoff <egoromanoff@mail.ru>
2022-12-08 10:20:46 +0300

    feat: add sharp

--- next.config.js
+++ next.config.js
@@ -8,16 +8,6 @@ const nextConfig = {
   images: {
     deviceSizes: [320, 575, 767, 991, 1150],
   }
-
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: ['@svgr/webpack'],
-  //   })
-
-  //   return config
-  // },
 }
 
 module.exports = nextConfig
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,24 +4,20 @@ import { ThemeProvider } from 'react-bootstrap'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import '../styles/globals.scss'
-// import { SSRProvider } from 'react-aria'
 
 export default function App({ Component, pageProps }) {
   return (
-    // <SSRProvider>
-      <Provider store={store}>
-        <ThemeProvider
-          breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs']}
-          minBreakpoint="xs"
-        >
-          <Header />
-            <main className="main">
-              <Component {...pageProps} />
-            </main>
-          <Footer />
-        </ThemeProvider>
-      </Provider>
-    // </SSRProvider>
-
+    <Provider store={store}>
+      <ThemeProvider
+        breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs']}
+        minBreakpoint="xs"
+      >
+        <Header />
+          <main className="main">
+            <Component {...pageProps} />
+          </main>
+        <Footer />
+      </ThemeProvider>
+    </Provider>
   )
 }
